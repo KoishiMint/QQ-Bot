3329f48cb8b9c7dc3a95042dc3d6922be89f1885
KoishiMint <koishi_mint@qq.com>
2021-02-04 09:40:02 +0800

    修复了MVP的若干bug

--- plugins/tool/manage.py
+++ plugins/tool/manage.py
@@ -5,7 +5,7 @@ from nonebot import on_command, CommandSession
 import config
 
 
-@on_command('开启MVP', aliases={'开启mvp'}, permission=GROUP_ADMIN | SUPERUSER)
+@on_command('开启MVP', aliases={'开启mvp'}, permission=SUPERUSER)
 async def _(session: CommandSession):
     if session.event.group_id not in config.QQ_GROUP:
         config.QQ_GROUP.insert(0, str(session.event.group_id))
@@ -14,7 +14,7 @@ async def _(session: CommandSession):
         await session.send('本群已在MVP列表中')
 
 
-@on_command('关闭MVP', aliases={'关闭mvp'}, permission=GROUP_ADMIN | SUPERUSER)
+@on_command('关闭MVP', aliases={'关闭mvp'}, permission=SUPERUSER)
 async def _(session: CommandSession):
     if session.event.group_id in config.QQ_GROUP:
         config.QQ_GROUP.remove(str(session.event.group_id))
