88c5f29a5dbcdcbc7dc1cf78bf90e63eb0c8b6e9
DuskPrimrose <zephyria.li@gmail.com>
2021-02-06 18:17:22 -0500

    Modify ursus reminder msg

--- plugins/tool/ursus_reminder.py
+++ plugins/tool/ursus_reminder.py
@@ -1,7 +1,14 @@
+"""
+提醒Ursus（564）小助手
+会在整点与30分钟时播报，以及还剩10分钟结束时的最后播报。
+如果双倍564时间有变化，请修改`URSUS_HOUR_LIST`与对应的`URSUS_LAST_HOUR_LIST`（24小时制）
+"""
 import nonebot
 from nonebot.permission import SUPERUSER
 from nonebot import on_command, CommandSession
+from config import NICKNAME
 
+NICKNAME = list(NICKNAME)[0]
 
 URSUS_TIMEZONE = 'Asia/Shanghai'
 URSUS_HOUR_LIST = "2, 3, 9, 10"
@@ -20,8 +27,8 @@ URSUS_GROUP_LIST = []
 async def _():
     if URSUS_GROUP_LIST:
         bot = nonebot.get_bot()
+        cqm = f'{NICKNAME}提醒：现在是双倍Ursus时间，请尽快通过次元镜完成每日三次Ursus。'
         for group in URSUS_GROUP_LIST:
-            cqm = '现在是双倍Ursus时间，请尽快完成。'
             await bot.send_group_msg(group_id=group, message=cqm)
 
 
@@ -35,19 +42,20 @@ async def _():
 async def _():
     if URSUS_GROUP_LIST:
         bot = nonebot.get_bot()
+        cqm = f'{NICKNAME}提醒：离双倍Ursus结束还剩10分钟，请尽快通过次元镜完成每日三次Ursus。'
         for group in URSUS_GROUP_LIST:
-            cqm = '离双倍Ursus结束还剩10分钟，请尽快完成。'
             await bot.send_group_msg(group_id=group, message=cqm)
 
 
+# Turn on/off reminder
 @on_command('开启564', aliases={'开启ursus'}, permission=SUPERUSER)
 async def _(session: CommandSession):
     global URSUS_GROUP_LIST
     if session.event.group_id not in URSUS_GROUP_LIST:
         URSUS_GROUP_LIST.append(session.event.group_id)
-        await session.send('已开启本群Ursus播报功能')
+        await session.send(f'{NICKNAME}已开启本群Ursus播报功能')
     else:
-        await session.send('本群Ursus播报已是开启状态')
+        await session.send(f'{NICKNAME}发现本群Ursus播报已是开启状态')
 
 
 @on_command('关闭564', aliases={'关闭ursus'}, permission=SUPERUSER)
@@ -55,6 +63,6 @@ async def _(session: CommandSession):
     global URSUS_GROUP_LIST
     if session.event.group_id in URSUS_GROUP_LIST:
         URSUS_GROUP_LIST.remove(session.event.group_id)
-        await session.send('已关闭本群Ursus播报功能')
+        await session.send(f'{NICKNAME}已关闭本群Ursus播报功能')
     else:
-        await session.send('本群Ursus播报已是关闭状态')
+        await session.send(f'{NICKNAME}发现本群Ursus播报已是关闭状态')
